96a0884f9707c88f6e6ac0165bb35642544b61b5
Mati <m.luzak@outlook.com>
2024-10-12 11:02:09 +0200

    dodanie Wolumin

---
+++ src/main/java/org/example/model/Wolumin.java
@@ -0,0 +1,48 @@
+package org.example.model;
+
+import org.example.excpetions.WoluminException;
+
+public class Wolumin {
+    private String wydawnictwo;
+    private String jezyk;
+    private String tytul;
+
+    public Wolumin(String wydawnictwo, String jezyk, String tytul) {
+        if(wydawnictwo.isEmpty()){
+            throw new WoluminException("Blendne Wydawnictwo");
+        }
+        if(jezyk.isEmpty()){
+            throw new WoluminException("Blendne jezyk");
+        }
+        if(tytul.isEmpty()){
+            throw new WoluminException("Blendne tytul");
+        }
+        this.wydawnictwo = wydawnictwo;
+        this.jezyk = jezyk;
+        this.tytul = tytul;
+    }
+
+    public String getWydawnictwo() {
+        return wydawnictwo;
+    }
+
+    public String getJezyk() {
+        return jezyk;
+    }
+
+    public String getTytul() {
+        return tytul;
+    }
+
+    public void setWydawnictwo(String wydawnictwo) {
+        this.wydawnictwo = wydawnictwo;
+    }
+
+    public void setJezyk(String jezyk) {
+        this.jezyk = jezyk;
+    }
+
+    public void setTytul(String tytul) {
+        this.tytul = tytul;
+    }
+}
